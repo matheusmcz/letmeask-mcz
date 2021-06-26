4abdf0c18a0e3c8517c8669455888128d3853aad
matheusmcz <matheusmczvieira@gmail.com>
2021-06-25 22:01:12 -0300

    terceiro dia

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+import React, { ButtonHTMLAttributes } from "react";
+import "../styles/button.scss";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+  return <button className="button" {...props} />;
+}
